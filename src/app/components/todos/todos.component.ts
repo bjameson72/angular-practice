04475af1df99c2e9539557ddf302ebbcec319c92
Brock Jameson <bjameson@talentpath.com>
2019-09-11 16:34:09 -0700

    dynamic todos implemented

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -25,4 +25,10 @@ export class TodosComponent implements OnInit {
     // Delete from Server
     this.todoService.deleteTodo(todo).subscribe();
   }
+
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    });
+  }
 }
