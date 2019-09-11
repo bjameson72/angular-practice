916368e50e385d27b1e2bf5b0719e45f5ffa0a21
Brock Jameson <bjameson@talentpath.com>
2019-09-11 16:01:55 -0700

    basic get put delete done with JSplaceholders

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { TodoService } from "../../services/todo.service";
+
+import { Todo } from "../../models/Todo";
+
+@Component({
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.css"]
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    // Delete from UI
+    this.todos = this.todos.filter(to => to.id !== todo.id);
+    // Delete from Server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+}
